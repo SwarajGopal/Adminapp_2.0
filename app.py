5e31460171815b703955ca76625e764104b0cb5b
Swaraj Gopal <swarajgopalv@gmail.com>
2023-05-24 19:52:56 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,121 @@
+from flask import Flask, render_template, redirect, url_for, request, Response
+import cv2
+import pickle
+import numpy as np
+import struct
+import socket
+import time
+from threading import Thread
+
+app = Flask(__name__)
+server_socket = None
+
+PORT = 63333
+
+
+def get_host_ip():
+    # Create a temporary socket to get the local IP address
+    temp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        temp_socket.connect(('8.8.8.8', 80))  # Connect to a public IP address
+        host_ip = temp_socket.getsockname()[0]  # Get the local IP address
+    finally:
+        temp_socket.close()
+    return host_ip
+
+
+HOST = get_host_ip()
+
+
+def receive_frames():
+    global server_socket
+
+    if server_socket is None:
+        while True:
+            try:
+                server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                server_socket.bind((HOST, PORT))
+                server_socket.listen(10)
+                print('Socket now listening')
+                break  # Break out of the loop if server socket is successfully created and listening
+            except OSError:
+                print('Failed to create server socket. Retrying in 5 seconds...')
+                time.sleep(5)
+
+    conn, addr = server_socket.accept()
+    print('Client connected:', addr)
+
+    try:
+        data = b""
+        payload_size = struct.calcsize(">L")
+        print("payload_size: {}".format(payload_size))
+        while True:
+            while len(data) < payload_size:
+                print("Recv: {}".format(len(data)))
+                data += conn.recv(4096)
+
+            print("Done Recv: {}".format(len(data)))
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack(">L", packed_msg_size)[0]
+            print("msg_size: {}".format(msg_size))
+            while len(data) < msg_size:
+                data += conn.recv(4096)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+
+            frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+            frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+
+            # Process the frame (if needed)
+
+            # Convert the frame to JPEG image
+            ret, jpeg = cv2.imencode('.jpg', frame)
+            frame_data = jpeg.tobytes()
+
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame_data + b'\r\n')
+    except (ConnectionResetError, ConnectionAbortedError):
+        print('Connection forcibly closed by the remote host. Restarting server...')
+        conn.close()
+        server_socket.close()
+        server_socket = None
+        yield from receive_frames()  # Restart the server
+    finally:
+        conn.close()
+
+
+def start_receive_frames_thread():
+    thread = Thread(target=receive_frames)
+    thread.daemon = True
+    thread.start()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(receive_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/adminlogin')
+def adminlogin():
+    return render_template('adminlogin.html')
+
+
+@app.route('/adminsignup')
+def adminsignup():
+    return render_template('adminsignup.html')
+
+
+@app.route('/admondashboard')
+def admindashboard():
+    return render_template('admindashboard.html', host=HOST, port=PORT)
+
+
+if __name__ == '__main__':
+    start_receive_frames_thread()  # Start the receive_frames() function in a separate thread
+    app.run(debug=True, port=5001)
---
+++ static/script/admindash.js
@@ -0,0 +1,192 @@
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all tab links and tab content elements
+  var tablinks = document.getElementsByClassName("tablinks");
+  var tabcontent = document.getElementsByClassName("tabcontent");
+
+  // Set the first tab as active by default
+  tablinks[0].classList.add("active");
+  tabcontent[0].classList.add("active");
+  tabcontent[0].classList.add("fade-in");
+
+  // Add click event listener to tab links
+  Array.from(tablinks).forEach(function (tablink) {
+      tablink.addEventListener("click", function () {
+          var targetTab = this.getAttribute("data-tab");
+
+          // Remove active class from all tab links and tab content elements
+          Array.from(tablinks).forEach(function (link) {
+              link.classList.remove("active");
+          });
+          Array.from(tabcontent).forEach(function (content) {
+              content.classList.remove("active");
+              content.classList.remove("fade-in");
+          });
+
+          // Add active class to clicked tab link and corresponding tab content
+          this.classList.add("active");
+          document.getElementById(targetTab).classList.add("active");
+          document.getElementById(targetTab).classList.add("fade-in");
+      });
+  });
+});
+
+// Firebase configuration
+var firebaseConfig = {
+  // Your Firebase configuration here
+  apiKey: "AIzaSyB_jLJFUB1L7C8n8-9PHu8sbmxyPoKIr5k",
+  authDomain: "proctor-watch.firebaseapp.com",
+  databaseURL: "https://proctor-watch-default-rtdb.firebaseio.com",
+  projectId: "proctor-watch",
+  storageBucket: "proctor-watch.appspot.com",
+  messagingSenderId: "375719442930",
+  appId: "1:375719442930:web:51fb94434165515540d36b"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
+
+// Function to retrieve and display all warning messages from Realtime Database
+function displayWarningMessages() {
+  db.child("warnings").on("value", (snapshot) => {
+    const warnings = snapshot.val();
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.innerHTML = ""; // Clear the sidebar before updating with new warnings
+
+    for (const key in warnings) {
+      if (warnings.hasOwnProperty(key)) {
+        const message = warnings[key].message;
+        const timestamp = warnings[key].timestamp;
+
+        const warningMessageElement = document.createElement("div");
+        warningMessageElement.classList.add("notification", "warning");
+
+        const messageElement = document.createElement("p");
+        messageElement.innerText = "Alert: " + message;
+        warningMessageElement.appendChild(messageElement);
+
+        const timestampElement = document.createElement("p");
+        timestampElement.innerText = "Time: " + timestamp;
+        warningMessageElement.appendChild(timestampElement);
+
+        sidebar.appendChild(warningMessageElement);
+      }
+    }
+  }, (error) => {
+    console.log("Error getting warning messages: ", error);
+  });
+}
+
+// Call the function to retrieve and display all warning messages on page load
+document.addEventListener("DOMContentLoaded", function () {
+  displayWarningMessages();
+});
+
+// Get a reference to the database service
+var database = firebase.database();
+
+// Get the input field and chatbox div
+var input = document.getElementById("usermsg");
+var chatbox = document.getElementById("chatbox");
+
+// Listen for form submit event
+document.getElementById("message-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  // Get the current user's name and message from input field
+  var user = "Admin";
+  var message = input.value;
+
+  // Create a new message reference in the database
+  var newMessageRef = database.ref("messages").push();
+  // Set the message data to the new reference
+  newMessageRef.set({
+      user: user,
+      message: message,
+  });
+
+  // Clear the input field
+  input.value = "";
+});
+
+// Listen for new messages in the database
+database.ref("messages").on("child_added", function (snapshot) {
+  // Get the message data
+  var data = snapshot.val();
+  var user = data.user;
+  var message = data.message;
+
+  // Create a new chat message element
+  var chatMessage = document.createElement("div");
+  chatMessage.innerHTML = "<b>" + user + "</b>: " + message;
+
+  // Apply fade-in effect to the chat message
+  chatMessage.style.opacity = 0;
+  chatMessage.style.animation = "fade-in 0.5s ease forwards";
+
+  // Append the message element to the chatbox
+  chatbox.appendChild(chatMessage);
+
+  // Scroll to the bottom of the chatbox
+  chatbox.scrollTop = chatbox.scrollHeight;
+});
+
+// Listen for logout event
+document.getElementById("logout-button").addEventListener("click", function () {
+  // Remove all chat messages from the database
+  database.ref("messages").remove();
+
+  // Clear the chatbox on the screen
+  chatbox.innerHTML = "";
+});
+
+function submitQuestions() {
+  var questions = [];
+  var questionInputs = document.querySelectorAll("input[id^='question']");
+
+  questionInputs.forEach(function (input, index) {
+    var question = {
+      questionNumber: index + 1,
+      questionText: input.value,
+      options: []
+    };
+
+    questions.push(question);
+  });
+
+  console.log(questions);
+
+  questionInputs.forEach(function (input) {
+    input.value = "";
+  });
+
+  var examRef = firebase.database().ref("exam/");
+  examRef.set(questions)
+    .then(function () {
+      console.log("Questions saved to the database.");
+      alert("Questions sent successfully!");
+    })
+    .catch(function (error) {
+      console.log("Error saving questions: ", error);
+      alert("An error occurred while sending questions. Please try again.");
+    });
+}
